0cc5e733c4d311c6d349160422b0bd87ae643caa
Sumit Prasad <sumitprasadd.wb24@gmail.com>
2024-12-14 20:06:32 +0530

    Html templates created and authentication setup

--- Login_System/Authentication/views.py
+++ Login_System/Authentication/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse(" I am working.")
+
+def signup(request):
+    return render(request, "Authentication/index.html" )
+
+def signin(request):
+    return render(request, "Authentication/signin.html" )
+
+def signout(request):
+    pass
