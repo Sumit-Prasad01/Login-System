b1eeb941c63ac330c39612be318facb50b06b043
Sumit Prasad <sumitprasadd.wb24@gmail.com>
2024-12-15 23:11:17 +0530

    Tailwind Css installed

--- Login_System/Login_System/settings.py
+++ Login_System/Login_System/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "tailwind",
+    "theme",
 ]
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = ['127.0.0.1']
+NPM_BIN_PATH = 'npm.cmd'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
