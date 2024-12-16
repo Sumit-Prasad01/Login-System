933163cb85ab3b6b06498b956507c71d909b202c
Sumit Prasad <sumitprasadd.wb24@gmail.com>
2024-12-17 00:24:10 +0530

    info modified

--- Login_System/Login_System/info.py
+++ Login_System/Login_System/info.py
@@ -1,5 +1,5 @@
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'testingacnt404@gmail.com'
+EMAIL_USE_TLS = ''
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
-EMAIL_PORT = 587
+EMAIL_PORT = ''
